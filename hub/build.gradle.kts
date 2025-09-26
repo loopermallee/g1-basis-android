145af6b7f4fd829a6daba2f41b45bfb5105f9ae8
loopermallee <whisperingwoods91@gmail.com>
2025-09-26 13:20:11 +0800

    Add Material and CardView dependencies

--- hub/build.gradle.kts
+++ hub/build.gradle.kts
@@ -54,6 +54,8 @@ dependencies {
     implementation(libs.androidx.lifecycle.livedata.ktx)
     implementation(libs.androidx.recyclerview)
     implementation(libs.androidx.constraintlayout)
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.cardview:cardview:1.0.0")
     implementation(libs.hilt.android)
     implementation(libs.nordic.ble)
     implementation(libs.nordic.scanner)
